916119ced86838e3f26a5ca82241dfdbec7714bc
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-08-30 15:09:01 +0900

    [HIMIN-87] feat : LoginRequest password validation 추가

--- src/main/java/com/prgrms/himin/member/dto/request/MemberLoginRequest.java
+++ src/main/java/com/prgrms/himin/member/dto/request/MemberLoginRequest.java
@@ -14,7 +14,8 @@ public final class MemberLoginRequest {
 	@Size(max = 20, message = "loginId는 최대 20글자입니다.")
 	@NotBlank(message = "loginId가 비어있으면 안됩니다.")
 	private final String loginId;
-	
+
+	@Size(max = 20, message = "loginId는 최대 20글자입니다.")
 	@NotBlank(message = "비밀번호가 비어있으면 안됩니다.")
 	private final String password;
 }
