0b8120fee127b0dae68e34cb2ee7d4bddf398808
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-08-30 17:17:52 +0900

    [NO-JIRA] refactor : Member grade필드 EnumType String으로 변경

--- src/main/java/com/prgrms/himin/member/domain/Member.java
+++ src/main/java/com/prgrms/himin/member/domain/Member.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -53,7 +54,7 @@ public class Member {
 	@Column(name = "birthday", nullable = false)
 	private LocalDate birthday;
 
-	@Enumerated
+	@Enumerated(EnumType.STRING)
 	@Column(name = "grade", nullable = false)
 	private Grade grade;
 
