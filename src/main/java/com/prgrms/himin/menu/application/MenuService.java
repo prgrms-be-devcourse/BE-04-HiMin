aa83fdcee4ebede814e608257f6e15f086a58e8b
Curry4182 <kangbk4182@naver.com>
2023-09-01 17:49:09 +0900

    [HIMIN-97] feat : 메뉴 옵션 삭제 기능 구현

--- src/main/java/com/prgrms/himin/menu/application/MenuService.java
+++ src/main/java/com/prgrms/himin/menu/application/MenuService.java
@@ -205,6 +205,40 @@ public class MenuService {
 		);
 	}
 
+	@Transactional(readOnly = false)
+	public void deleteMenuOption(
+		Long shopId,
+		Long menuId,
+		Long menuOptionGroupId,
+		Long optionId
+	) {
+		Menu menu = menuRepository.findById(menuId)
+			.orElseThrow(() -> new RuntimeException("메뉴를 찾을 수 없습니다."));
+
+		checkShopId(
+			shopId,
+			menu
+		);
+
+		MenuOptionGroup menuOptionGroup = menuOptionGroupRepository.findById(menuOptionGroupId)
+			.orElseThrow(() -> new RuntimeException("메뉴 옵션 그룹 id를 찾을 수 없습니다."));
+
+		checkMenuId(
+			menuId,
+			menuOptionGroup
+		);
+
+		MenuOption menuOption = menuOptionRepository.findById(optionId)
+			.orElseThrow(() -> new RuntimeException("메뉴 옵션 id를 찾을 수 없습니다."));
+
+		checkMenuOptionGroupId(
+			menuOptionGroupId,
+			menuOption
+		);
+
+		menuOptionRepository.delete(menuOption);
+	}
+
 	private void checkShopId(
 		Long shopId,
 		Menu menu
