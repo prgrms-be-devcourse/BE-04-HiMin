48e740a90c364d3b39360539cf6b8a90cbe17fa5
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-01 09:18:20 +0900

    [HIMIN-114] refactor: Enum Validator 어노테이션 정리

--- src/main/java/com/prgrms/himin/global/common/ValidEnum.java
+++ src/main/java/com/prgrms/himin/global/common/ValidEnum.java
@@ -2,14 +2,16 @@ package com.prgrms.himin.global.common;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
 @Constraint(validatedBy = EnumValidator.class)
-@Target({ElementType.FIELD, ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
     String message() default "유효하지 않은 Enum 입니다.";
 
