baba50bf0a41ce8b3da8efbd38989f23a4063eba
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-02 17:04:00 +0900

    [HIMIN-130] refactor: collect(Collectors.toList()) → toList() 로 변경

--- src/main/java/com/prgrms/himin/shop/application/ShopService.java
+++ src/main/java/com/prgrms/himin/shop/application/ShopService.java
@@ -1,7 +1,6 @@
 package com.prgrms.himin.shop.application;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -56,7 +55,7 @@ public class ShopService {
 
 		return shops.stream()
 			.map(ShopResponse::from)
-			.collect(Collectors.toList());
+			.toList();
 	}
 
 	@Transactional
