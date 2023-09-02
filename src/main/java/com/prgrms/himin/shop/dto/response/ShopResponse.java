dfee3034872b72d90b4f9828f29d8606a86d3bcd
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-09-02 13:11:49 +0900

    [HIMIN-119] refactor : Shop dto 레코드로 변경

--- src/main/java/com/prgrms/himin/shop/dto/response/ShopResponse.java
+++ src/main/java/com/prgrms/himin/shop/dto/response/ShopResponse.java
@@ -3,74 +3,37 @@ package com.prgrms.himin.shop.dto.response;
 import com.prgrms.himin.shop.domain.Category;
 import com.prgrms.himin.shop.domain.Shop;
 import com.prgrms.himin.shop.domain.ShopStatus;
+
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public final class ShopResponse {
-
-    private final Long shopId;
-
-    private final String name;
-
-    private final Category category;
-
-    private final String address;
-
-    private final String phone;
-
-    private final String content;
-
-    private final int deliveryTip;
-
-    private final int dibsCount;
-
-    private final ShopStatus status;
-
-    private final String openingTime;
-
-    private final String closingTime;
-
-    @Builder
-    private ShopResponse(
-            Long shopId,
-            String name,
-            Category category,
-            String address,
-            String phone,
-            String content,
-            int deliveryTip,
-            int dibsCount,
-            ShopStatus status,
-            String openingTime,
-            String closingTime
-    ) {
-        this.shopId = shopId;
-        this.name = name;
-        this.category = category;
-        this.address = address;
-        this.phone = phone;
-        this.content = content;
-        this.deliveryTip = deliveryTip;
-        this.dibsCount = dibsCount;
-        this.status = status;
-        this.openingTime = openingTime;
-        this.closingTime = closingTime;
-    }
+@Builder
+public record ShopResponse(
+    Long shopId,
+    String name,
+    Category category,
+    String address,
+    String phone,
+    String content,
+    int deliveryTip,
+    int dibsCount,
+    ShopStatus status,
+    String openingTime,
+    String closingTime
+) {
 
     public static ShopResponse from(Shop shop) {
         return ShopResponse.builder()
-                .shopId(shop.getShopId())
-                .name(shop.getName())
-                .category(shop.getCategory())
-                .address(shop.getAddress())
-                .phone(shop.getPhone())
-                .content(shop.getContent())
-                .deliveryTip(shop.getDeliveryTip())
-                .dibsCount(shop.getDibsCount())
-                .status(shop.getStatus())
-                .openingTime(shop.getOpeningTime())
-                .closingTime(shop.getClosingTime())
-                .build();
+            .shopId(shop.getShopId())
+            .name(shop.getName())
+            .category(shop.getCategory())
+            .address(shop.getAddress())
+            .phone(shop.getPhone())
+            .content(shop.getContent())
+            .deliveryTip(shop.getDeliveryTip())
+            .dibsCount(shop.getDibsCount())
+            .status(shop.getStatus())
+            .openingTime(shop.getOpeningTime())
+            .closingTime(shop.getClosingTime())
+            .build();
     }
 }
