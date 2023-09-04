6007e3a2c73db2178bc0dd5109ba157d75791726
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-04 20:53:26 +0900

    [HIMIN-131] test: 가게 test fixture 추가 (domain, request)

---
+++ src/test/java/com/prgrms/himin/setup/request/ShopCreateRequestBuilder.java
@@ -0,0 +1,36 @@
+package com.prgrms.himin.setup.request;
+
+import com.prgrms.himin.shop.domain.Category;
+import com.prgrms.himin.shop.dto.request.ShopCreateRequest;
+
+public class ShopCreateRequestBuilder {
+
+	public static ShopCreateRequest successBuild() {
+		return new ShopCreateRequest(
+			"맥도날드",
+			Category.FAST_FOOD.toString(),
+			"경기도 광명시 광명동",
+			"02-2611-2222",
+			"안녕하세요. 맥도날드입니다.",
+			1000,
+			"09:00",
+			"21:00"
+		);
+	}
+
+	public static ShopCreateRequest failBuild(
+		String name,
+		Category category
+	) {
+		return new ShopCreateRequest(
+			name,
+			category.toString(),
+			"경기도 광명시 광명동",
+			"02-2611-2222",
+			"안녕하세요. %s입니다.".formatted(name),
+			1000,
+			"09:00",
+			"21:00"
+		);
+	}
+}
