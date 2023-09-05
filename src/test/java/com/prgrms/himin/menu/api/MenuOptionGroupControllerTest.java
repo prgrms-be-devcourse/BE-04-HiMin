705361757d28e922963bfaf2cbcb3c2b684f6c88
Curry4182 <kangbk4182@naver.com>
2023-09-05 17:04:15 +0900

    [HIMIN-148] test : 메뉴 옵션 그룹 수정 테스트 구현

--- src/test/java/com/prgrms/himin/menu/api/MenuOptionGroupControllerTest.java
+++ src/test/java/com/prgrms/himin/menu/api/MenuOptionGroupControllerTest.java
@@ -1,10 +1,12 @@
 package com.prgrms.himin.menu.api;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -25,14 +27,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.himin.global.error.exception.ErrorCode;
 import com.prgrms.himin.menu.domain.Menu;
+import com.prgrms.himin.menu.domain.MenuOptionGroup;
+import com.prgrms.himin.menu.domain.MenuOptionGroupRepository;
 import com.prgrms.himin.menu.domain.MenuRepository;
 import com.prgrms.himin.menu.dto.request.MenuCreateRequest;
 import com.prgrms.himin.menu.dto.request.MenuOptionGroupCreateRequest;
+import com.prgrms.himin.menu.dto.request.MenuOptionGroupUpdateRequest;
 import com.prgrms.himin.setup.domain.MenuOptionGroupSetUp;
 import com.prgrms.himin.setup.domain.MenuSetUp;
 import com.prgrms.himin.setup.domain.ShopSetUp;
 import com.prgrms.himin.setup.request.MenuCreateRequestBuilder;
 import com.prgrms.himin.setup.request.MenuOptionGroupRequestBuilder;
+import com.prgrms.himin.setup.request.MenuOptionGroupUpdateRequestBuilder;
 import com.prgrms.himin.shop.domain.Shop;
 
 @SpringBootTest
@@ -59,6 +65,9 @@ public class MenuOptionGroupControllerTest {
 	@Autowired
 	MenuOptionGroupSetUp menuOptionGroupSetUp;
 
+	@Autowired
+	MenuOptionGroupRepository menuOptionGroupRepository;
+
 	Shop shop;
 
 	Menu menu;
@@ -75,7 +84,8 @@ public class MenuOptionGroupControllerTest {
 
 		private static Stream<Arguments> provideRequestForErrorValue() {
 			return Stream.of(
-				Arguments.of("마라 칠리 매운 허니버터 와사비 치즈 하바네로 맛 150도 오븐에서 30분이 상 구운 치킨 오븐구이", "메뉴 옵션 그룹 이름은 최대 30글자 입니다."),
+				Arguments.of("마라 칠리 매운 허니버터 와사비 치즈 하바네로 맛 150도 오븐에서 30분이상 구운 치킨 오븐구이 주방장 선택",
+					"메뉴 옵션 그룹 이름은 최대 30글자 입니다."),
 				Arguments.of(null, "메뉴 옵션 그룹 이름은 비어있으면 안됩니다."),
 				Arguments.of("", "메뉴 옵션 그룹 이름은 비어있으면 안됩니다."),
 				Arguments.of("  ", "메뉴 옵션 그룹 이름은 비어있으면 안됩니다.")
@@ -120,6 +130,85 @@ public class MenuOptionGroupControllerTest {
 					BASE_URL,
 					shop.getShopId(),
 					menu.getId()
+				)
+					.content(body)
+					.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print());
+
+			// then
+			resultActions.andExpect(status().isBadRequest())
+				.andExpect((result) -> assertTrue(
+					result.getResolvedException().getClass().isAssignableFrom((MethodArgumentNotValidException.class))))
+				.andExpect(jsonPath("error").value(ErrorCode.INVALID_REQUEST.toString()))
+				.andExpect(jsonPath("errors[0].field").value("name"))
+				.andExpect(jsonPath("errors[0].value").value(input))
+				.andExpect(jsonPath("errors[0].reason").value(expected))
+				.andExpect(jsonPath("code").value(ErrorCode.INVALID_REQUEST.getCode()))
+				.andExpect(jsonPath("message").value(ErrorCode.INVALID_REQUEST.getMessage()));
+		}
+	}
+
+	@Nested
+	@DisplayName("메뉴 옵션 그룹 수정을 할 수 있다.")
+	class UpdateMenuOptionGroup {
+
+		final String UPDATE_URL = BASE_URL + "/{menuOptionGroupId}";
+
+		private static Stream<Arguments> provideRequestForErrorValue() {
+			return Stream.of(
+				Arguments.of("마라 칠리 매운 허니버터 와사비 치즈 하바네로 맛 150도 오븐에서 30분이상 구운 치킨 오븐구이 주방장 선택",
+					"메뉴 옵션 그룹 이름은 최대 30글자 입니다."),
+				Arguments.of(null, "메뉴 옵션 그룹 이름은 비어있으면 안됩니다."),
+				Arguments.of("", "메뉴 옵션 그룹 이름은 비어있으면 안됩니다."),
+				Arguments.of("  ", "메뉴 옵션 그룹 이름은 비어있으면 안됩니다.")
+			);
+		}
+
+		@Test
+		@DisplayName("성공한다.")
+		void success_test() throws Exception {
+			// given
+			MenuOptionGroup savedMenuOptionGroup = menuOptionGroupSetUp.saveOne(menu);
+
+			MenuOptionGroupUpdateRequest request = MenuOptionGroupUpdateRequestBuilder.successBuild();
+			String body = objectMapper.writeValueAsString(request);
+
+			// when
+			ResultActions resultActions = mvc.perform(put(
+					UPDATE_URL,
+					shop.getShopId(),
+					menu.getId(),
+					savedMenuOptionGroup.getId()
+				)
+					.content(body)
+					.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print());
+
+			// then
+			resultActions.andExpect(status().isNoContent());
+			Optional<MenuOptionGroup> maySavedMenuOptionGroup = menuOptionGroupRepository.findById(
+				savedMenuOptionGroup.getId());
+			assertThat(maySavedMenuOptionGroup.isPresent()).isTrue();
+			savedMenuOptionGroup = maySavedMenuOptionGroup.get();
+			assertThat(savedMenuOptionGroup.getName()).isEqualTo(request.name());
+		}
+
+		@ParameterizedTest
+		@MethodSource("provideRequestForErrorValue")
+		@DisplayName("실패한다.")
+		void fail_test(String input, String expected) throws Exception {
+			// given
+			MenuOptionGroup savedMenuOptionGroup = menuOptionGroupSetUp.saveOne(menu);
+
+			MenuOptionGroupUpdateRequest request = MenuOptionGroupUpdateRequestBuilder.failBuild(input);
+			String body = objectMapper.writeValueAsString(request);
+
+			// when
+			ResultActions resultActions = mvc.perform(put(
+					UPDATE_URL,
+					shop.getShopId(),
+					menu.getId(),
+					savedMenuOptionGroup.getId()
 				)
 					.content(body)
 					.contentType(MediaType.APPLICATION_JSON))
---
+++ src/test/java/com/prgrms/himin/setup/request/MenuOptionGroupUpdateRequestBuilder.java
@@ -0,0 +1,18 @@
+package com.prgrms.himin.setup.request;
+
+import com.prgrms.himin.menu.dto.request.MenuOptionGroupUpdateRequest;
+
+public class MenuOptionGroupUpdateRequestBuilder {
+
+	public static MenuOptionGroupUpdateRequest successBuild() {
+		return new MenuOptionGroupUpdateRequest(
+			"주방장 선택"
+		);
+	}
+
+	public static MenuOptionGroupUpdateRequest failBuild(String name) {
+		return new MenuOptionGroupUpdateRequest(
+			name
+		);
+	}
+}
