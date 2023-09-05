ae5a7cfaa3dbf3174a02f50c3c1bd1d1bc20dd2c
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-05 10:18:47 +0900

    [HIMIN-131] test: 가게 검색 조건에 따른 다건 조회 테스트 작성

--- src/test/java/com/prgrms/himin/shop/api/ShopControllerTest.java
+++ src/test/java/com/prgrms/himin/shop/api/ShopControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
@@ -182,6 +183,77 @@ class ShopControllerTest {
 				.andExpect(jsonPath("error").value(ErrorCode.SHOP_NOT_FOUND.toString()))
 				.andExpect(jsonPath("code").value(ErrorCode.SHOP_NOT_FOUND.getCode()))
 				.andExpect(jsonPath("message").value(ErrorCode.SHOP_NOT_FOUND.getMessage()));
+		}
+	}
+
+	@Nested
+	@DisplayName("가게 목록을 조회할 수 있다.")
+	class GetShops {
+
+		@DisplayName("전체를 조회한다.")
+		@Test
+		void success_test1() throws Exception {
+			// when
+			List<Shop> shops = shopSetUp.saveMany(2);
+
+			ResultActions resultAction = mvc.perform(get(BASE_URL))
+				.andDo(print());
+
+			// then
+			resultAction.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("[0].shopId").value(shops.get(0).getShopId()))
+				.andExpect(jsonPath("[0].name").value(shops.get(0).getName()))
+				.andExpect(jsonPath("[0].category").value(shops.get(0).getCategory().toString()))
+				.andExpect(jsonPath("[0].address").value(shops.get(0).getAddress()))
+				.andExpect(jsonPath("[0].phone").value(shops.get(0).getPhone()))
+				.andExpect(jsonPath("[0].content").value(shops.get(0).getContent()))
+				.andExpect(jsonPath("[0].deliveryTip").value(shops.get(0).getDeliveryTip()))
+				.andExpect(jsonPath("[0].dibsCount").value(shops.get(0).getDibsCount()))
+				.andExpect(jsonPath("[0].status").value(shops.get(0).getStatus().toString()))
+				.andExpect(jsonPath("[0].openingTime").value(shops.get(0).getOpeningTime()))
+				.andExpect(jsonPath("[0].closingTime").value(shops.get(0).getClosingTime()))
+				.andExpect(jsonPath("[1].shopId").value(shops.get(1).getShopId()))
+				.andExpect(jsonPath("[1].name").value(shops.get(1).getName()))
+				.andExpect(jsonPath("[1].category").value(shops.get(1).getCategory().toString()))
+				.andExpect(jsonPath("[1].address").value(shops.get(1).getAddress()))
+				.andExpect(jsonPath("[1].phone").value(shops.get(1).getPhone()))
+				.andExpect(jsonPath("[1].content").value(shops.get(1).getContent()))
+				.andExpect(jsonPath("[1].deliveryTip").value(shops.get(1).getDeliveryTip()))
+				.andExpect(jsonPath("[1].dibsCount").value(shops.get(1).getDibsCount()))
+				.andExpect(jsonPath("[1].status").value(shops.get(1).getStatus().toString()))
+				.andExpect(jsonPath("[1].openingTime").value(shops.get(1).getOpeningTime()))
+				.andExpect(jsonPath("[1].closingTime").value(shops.get(1).getClosingTime()));
+		}
+
+		@DisplayName("검색 조건에 따라 검색할 수 있다.")
+		@Test
+		void success_test2() throws Exception {
+			// given
+			List<Shop> shops = shopSetUp.saveMany(2);
+
+			String name = "광명2동";
+
+			// when
+			ResultActions resultAction = mvc.perform(get(BASE_URL)
+					.queryParam("name", name)
+				)
+				.andDo(print());
+
+			// then
+			resultAction.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("[0].shopId").value(shops.get(1).getShopId()))
+				.andExpect(jsonPath("[0].name").value(shops.get(1).getName()))
+				.andExpect(jsonPath("[0].category").value(shops.get(1).getCategory().toString()))
+				.andExpect(jsonPath("[0].address").value(shops.get(1).getAddress()))
+				.andExpect(jsonPath("[0].phone").value(shops.get(1).getPhone()))
+				.andExpect(jsonPath("[0].content").value(shops.get(1).getContent()))
+				.andExpect(jsonPath("[0].deliveryTip").value(shops.get(1).getDeliveryTip()))
+				.andExpect(jsonPath("[0].dibsCount").value(shops.get(1).getDibsCount()))
+				.andExpect(jsonPath("[0].status").value(shops.get(1).getStatus().toString()))
+				.andExpect(jsonPath("[0].openingTime").value(shops.get(1).getOpeningTime()))
+				.andExpect(jsonPath("[0].closingTime").value(shops.get(1).getClosingTime()));
 		}
 	}
 
