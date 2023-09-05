5efadd8fdaedb99d63321274e5393cc5410c8d31
Curry4182 <kangbk4182@naver.com>
2023-09-05 17:03:00 +0900

    [NO-JIRA] style : 메뉴 옵션 그룹 이름 생성과 수정 에러 메시지 통일

--- src/main/java/com/prgrms/himin/menu/dto/request/MenuOptionGroupUpdateRequest.java
+++ src/main/java/com/prgrms/himin/menu/dto/request/MenuOptionGroupUpdateRequest.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Size;
 
 public record MenuOptionGroupUpdateRequest(
 	@Size(max = 30, message = "메뉴 옵션 그룹 이름은 최대 30글자 입니다.")
-	@NotBlank(message = "메뉴 옵션 그룹 이름이 비어있으면 안됩니다.")
+	@NotBlank(message = "메뉴 옵션 그룹 이름은 비어있으면 안됩니다.")
 	String name
 ) {
 }
