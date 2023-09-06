05ff7cd16fa06b237e923ee18792cf41b808a8d2
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-09-06 11:18:06 +0900

    [HIMIN-151] feat : Addres update를 위한 dto 추가

---
+++ src/main/java/com/prgrms/himin/member/dto/request/AddressUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.prgrms.himin.member.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record AddressUpdateRequest(
+	@Size(max = 10, message = "주소별칭은 최대 10글자 입니다.")
+	@NotBlank(message = "주소별칭이 비어있으면 안됩니다.")
+	String addressAlias,
+
+	@Size(max = 50, message = "주소는 최대 50글자 입니다.")
+	@NotBlank(message = "주소는 비어있으면 안됩니다.")
+	String address
+) {
+}
