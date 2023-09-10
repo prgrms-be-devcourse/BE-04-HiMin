4f3b80b7589797e83e8741044838ccd3f288a927
Curry4182 <kangbk4182@naver.com>
2023-09-10 21:48:57 +0900

    [HIMIN-176] feat : 마지막 페이지인 경우 요청 멤버 아이디를 가진 주문중에서 마지막 주문 아이디를 커서로 반환

--- src/main/java/com/prgrms/himin/order/domain/OrderRepository.java
+++ src/main/java/com/prgrms/himin/order/domain/OrderRepository.java
@@ -3,4 +3,6 @@ package com.prgrms.himin.order.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
+
+	Order findFirstByMember_IdOrderByOrderIdDesc(Long member_id);
 }
