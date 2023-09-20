142939596bf03b06e1693156ce3a2a4940f4e5d9
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-20 22:50:21 +0900

    [HIMIN-229] test: 가게 단건 조회 테스트 추가

--- src/test/java/com/prgrms/himin/shop/docs/ShopDocumentationTest.java
+++ src/test/java/com/prgrms/himin/shop/docs/ShopDocumentationTest.java
@@ -5,6 +5,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -71,6 +72,39 @@ class ShopDocumentationTest {
 					fieldWithPath("deliveryTip").type(JsonFieldType.NUMBER).description("배달팁"),
 					fieldWithPath("openingTime").type(JsonFieldType.STRING).description("오픈 시간"),
 					fieldWithPath("closingTime").type(JsonFieldType.STRING).description("폐점 시간")
+				),
+				responseFields(
+					fieldWithPath("shopId").type(JsonFieldType.NUMBER).description("가게 ID"),
+					fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+					fieldWithPath("category").type(JsonFieldType.STRING).description("음식 카테고리"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("주소"),
+					fieldWithPath("phone").type(JsonFieldType.STRING).description("전화번호"),
+					fieldWithPath("content").type(JsonFieldType.STRING).description("소개글"),
+					fieldWithPath("deliveryTip").type(JsonFieldType.NUMBER).description("배달팁"),
+					fieldWithPath("dibsCount").type(JsonFieldType.NUMBER).description("찜 수"),
+					fieldWithPath("status").type(JsonFieldType.STRING).description("상태"),
+					fieldWithPath("openingTime").type(JsonFieldType.STRING).description("오픈 시간"),
+					fieldWithPath("closingTime").type(JsonFieldType.STRING).description("폐점 시간")
+				)));
+	}
+
+	@Test
+	@DisplayName("가게를 조회할 수 있다.")
+	void GetShop() throws Exception {
+		// given
+		ShopResponse response = ShopResponseBuilder.successBuild();
+
+		given(shopService.getShop(anyLong())).willReturn(response);
+
+		// when
+		ResultActions resultAction = mvc.perform(get("/api/shops/{shopId}", 1L));
+
+		// then
+		resultAction.andExpect(status().isOk())
+			.andDo(document("get-shop",
+				preprocessResponse(prettyPrint()),
+				pathParameters(
+					parameterWithName("shopId").description("가게 ID")
 				),
 				responseFields(
 					fieldWithPath("shopId").type(JsonFieldType.NUMBER).description("가게 ID"),
