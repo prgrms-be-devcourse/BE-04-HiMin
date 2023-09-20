d8d9e39343a4ab0aa718a98475c1660b18446c7b
Yiseul Park <omjl5123@catholic.ac.kr>
2023-09-21 00:21:17 +0900

    [HIMIN-229] test: 가게 정보 수정 테스트 추가

--- src/test/java/com/prgrms/himin/shop/docs/ShopDocumentationTest.java
+++ src/test/java/com/prgrms/himin/shop/docs/ShopDocumentationTest.java
@@ -2,10 +2,12 @@ package com.prgrms.himin.shop.docs;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,10 +24,12 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.himin.setup.request.ShopCreateRequestBuilder;
+import com.prgrms.himin.setup.request.ShopUpdateRequestBuilder;
 import com.prgrms.himin.setup.response.ShopResponseBuilder;
 import com.prgrms.himin.shop.api.ShopController;
 import com.prgrms.himin.shop.application.ShopService;
 import com.prgrms.himin.shop.dto.request.ShopCreateRequest;
+import com.prgrms.himin.shop.dto.request.ShopUpdateRequest;
 import com.prgrms.himin.shop.dto.response.ShopResponse;
 
 @AutoConfigureRestDocs
@@ -116,6 +120,36 @@ class ShopDocumentationTest {
 					fieldWithPath("deliveryTip").type(JsonFieldType.NUMBER).description("배달팁"),
 					fieldWithPath("dibsCount").type(JsonFieldType.NUMBER).description("찜 수"),
 					fieldWithPath("status").type(JsonFieldType.STRING).description("상태"),
+					fieldWithPath("openingTime").type(JsonFieldType.STRING).description("오픈 시간"),
+					fieldWithPath("closingTime").type(JsonFieldType.STRING).description("폐점 시간")
+				)));
+	}
+
+	@DisplayName("가게 정보를 수정할 수 있다.")
+	@Test
+	void updateShop() throws Exception {
+		// given
+		ShopUpdateRequest.Info request = ShopUpdateRequestBuilder.infoSuccessBuild();
+
+		willDoNothing().given(shopService).updateShop(anyLong(), any(ShopUpdateRequest.Info.class));
+
+		// when
+		ResultActions resultAction = mvc.perform(put("/api/shops/{shopId}", 1L)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))
+		);
+
+		// then
+		resultAction.andExpect(status().isNoContent())
+			.andDo(document("update-shop",
+				preprocessRequest(prettyPrint()),
+				requestFields(
+					fieldWithPath("name").type(JsonFieldType.STRING).description("가게 이름"),
+					fieldWithPath("category").type(JsonFieldType.STRING).description("음식 카테고리"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("주소"),
+					fieldWithPath("phone").type(JsonFieldType.STRING).description("전화번호"),
+					fieldWithPath("content").type(JsonFieldType.STRING).description("소개글"),
+					fieldWithPath("deliveryTip").type(JsonFieldType.NUMBER).description("배달팁"),
 					fieldWithPath("openingTime").type(JsonFieldType.STRING).description("오픈 시간"),
 					fieldWithPath("closingTime").type(JsonFieldType.STRING).description("폐점 시간")
 				)));
