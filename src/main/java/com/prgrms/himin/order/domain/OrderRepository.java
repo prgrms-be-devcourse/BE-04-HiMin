3fd648967e138ee970927586fc876a9044f6d1b9
Curry4182 <kangbk4182@naver.com>
2023-09-22 16:32:36 +0900

    [NO-ZIRA] refactor : 사용하지 않은 jpa 쿼리 삭제

--- src/main/java/com/prgrms/himin/order/domain/OrderRepository.java
+++ src/main/java/com/prgrms/himin/order/domain/OrderRepository.java
@@ -3,6 +3,4 @@ package com.prgrms.himin.order.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
-
-	Order findFirstByMemberIdOrderByOrderIdDesc(Long member_id);
 }
